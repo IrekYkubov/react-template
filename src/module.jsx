9fff3a0b02ef4278e7e712cf8c5ec748d10571db
Irek Yakubov <irekyyakubov@gmail.com>
2022-10-15 17:25:42 +0400

    Базовая сборка реакт

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import yku from './img/dvor.gif';
+
+const Module = () => (
+    <>
+        <h1>Hello React!</h1>
+        <img width="400" src={yku} />
+    </>
+);
+
+export default Module;
